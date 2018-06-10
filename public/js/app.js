9cb3b4e2cb38d3b840cccaeb4d6296a53a0ac735
gabriele.lana <gabriele.lana@cleancode.it>
2018-06-10 18:12:13 +0200

    Demo with two users and a private channel

--- public/js/app.js
+++ public/js/app.js
@@ -1,91 +1,88 @@
-$(() => {
-  const username = 'gabriele'
-  const password = 'secret'
-
-  // TODO: extract login function
+function login(username, password, callback) {
   superagent
     .post('/login')
     .send({username, password})
     .end((err, res) => {
+      console.log('login', err, res)
       if (res.status === 200) {
-        console.log('login', res)
         const token = res.body.token
         const socket = io({query: {token}})
-
-        superagent
-          .post('/rooms')
-          .send({token, name: 'gossip', topic: 'something secret', isPrivate: true})
-          .end((err, res) => {
-            console.log('create a private room', res)
-            setTimeout(
-              () => {
-                superagent
-                  .post('/messages')
-                  .send({token, message: 'this is between us', room: 'gossip'})
-                  .end((err, res) => {
-                    console.log('post to a private room', res)
-                  })
-              },
-              5000
-            )
-          })
-
-
-        $('form').submit(() => {
-          const message = $('#m').val()
-          superagent
-            .post('/messages')
-            .send({token, message})
-            .end((err, res) => {
-              $('#m').val('')
-            })
-
-          return false
-        })
-
-        socket.on('messages', data => {
-          $('#messages').append($('<li>').text(data.room + ':' + data.username + '> ' + data.message))
-        })
+        return callback(null, {token, socket})
       }
-
-      // // login again
-      // setTimeout(
-      //   () => {
-      //     console.log('login again')
-      //     superagent
-      //       .post('/login')
-      //       .send({username, password})
-      //       .end((err, res) => {
-      //         console.log('login', res)
-      //       })
-      //   },
-      //   5000
-      // )
-
-      // setTimeout(
-      //   () => {
-      //     console.log('login again')
-      //     superagent
-      //       .post('/login')
-      //       .send({username, password: 'wrong password'})
-      //       .end((err, res) => {
-      //         console.log('login', res)
-      //       })
-      //   },
-      //   10000
-      // )
-
-      // setTimeout(
-      //   () => {
-      //     console.log('anonymous loging')
-      //     superagent
-      //       .post('/login')
-      //       .send()
-      //       .end((err, res) => {
-      //         console.log('login', res)
-      //       })
-      //   },
-      //   15000
-      // )
+      return callback('Unable to login')
     })
+}
+
+function createRoom(token, name, topic, isPrivate, callback) {
+    superagent
+    .post('/rooms')
+    .send({token, name, topic, isPrivate})
+    .end((err, res) => {
+      console.log('create-room', err, res)
+      callback(err)
+    })
+}
+
+function postMessage(token, room, message, callback) {
+  superagent
+    .post('/messages')
+    .send({token, room, message})
+    .end((err, res) => {
+      console.log('post-message', err, res)
+      callback(err)
+    })
+}
+
+function inviteUser(token, room, username, callback) {
+  superagent
+    .post('/rooms/' + room + '/users')
+    .send({token, username})
+    .end((err, res) => {
+      console.log('invite-user', err, res)
+      callback(err)
+    })
+}
+
+
+$(() => {
+
+  login('gabriele', 'secret', (err, {token, socket}) => {
+    if (err) return console.log(err)
+
+    createRoom(token, 'gossip', 'gossip', true, (err) => {
+      if (err) return err
+      inviteUser(token, 'gossip', 'chiara', (err) => {
+        if (err) return err
+        postMessage(token, 'gossip', 'this must stay between us', (err) => {
+          if (err) return err
+        })
+      })
+    })
+
+    $('form').submit(() => {
+      const message = $('#m').val()
+      postMessage(token, 'main', message, (err) => {
+        $('#m').val('')
+      })
+      return false
+    })
+
+    socket.on('messages', data => {
+      $('#messages').append($('<li>').text(data.room + ':' + data.username + '> ' + data.message))
+    })
+  })
+
+  login('chiara', 'secret', (err, {token, socket}) => {
+    if (err) return console.log(err)
+
+    setTimeout(
+      () => {
+        postMessage(token, 'gossip', 'sure thing ;-)', (err) => {
+          if (err) return err
+        })
+      },
+      5000
+    )
+  })
+
 })
