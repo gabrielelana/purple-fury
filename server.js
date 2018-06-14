b5f8c0ee6e501f7cad5b72f5576dabd78ba0aaef
gabriele.lana <gabriele.lana@cleancode.it>
2018-06-14 13:02:32 +0200

    Linting

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const server = require('http').Server(app)
 const cors = require('cors')
 const io = require('socket.io')(server)
-const { check, body, validationResult } = require('express-validator/check');
+const { body, validationResult } = require('express-validator/check')
 
 const Database = require('nedb')
 const users = new Database({inMemoryOnly: true, timestampData: true})
@@ -21,7 +21,7 @@ app.use(require('body-parser').json())
 app.use(require('cookie-parser')())
 app.use(cors())
 
-function login(credentials, users, callback) {
+function login (credentials, users, callback) {
   const saltRounds = 5
   users.findOne({username: credentials.username}, (err, user) => {
     if (err) return callback(err)
@@ -33,17 +33,17 @@ function login(credentials, users, callback) {
           return callback(null, user)
         }
         console.log('wrong password')
-        return callback({error: 'wrong-password'})
+        return callback(new Error('Wrong password'))
       })
     } else {
-      bcrypt.hash(credentials.password, saltRounds, function(err, hashedPassword) {
+      bcrypt.hash(credentials.password, saltRounds, (err, hashedPassword) => {
         if (err) return callback(err)
         const user = {
           username: credentials.username,
           password: hashedPassword,
           rooms: [],
           preferences: {},
-          profile: {},
+          profile: {}
         }
         users.insert(user, (err, user) => {
           if (err) return callback(err)
@@ -55,7 +55,7 @@ function login(credentials, users, callback) {
   })
 }
 
-function credentials(credentials, _users, callback) {
+function credentials (credentials, _users, callback) {
   if (credentials.username && credentials.password) {
     return callback(null, credentials)
   }
@@ -70,7 +70,7 @@ function credentials(credentials, _users, callback) {
   })
 }
 
-function authenticate(req, res, next) {
+function authenticate (req, res, next) {
   const token = req.body.token || req.query.token || req.cookies.token
   if (!token) return res.status(401).json({error: 'Missing authentication token'})
   users.findOne({_id: token}, (err, user) => {
@@ -81,8 +81,8 @@ function authenticate(req, res, next) {
   })
 }
 
-function validate(req, res, next) {
-  const errors = validationResult(req);
+function validate (req, res, next) {
+  const errors = validationResult(req)
   if (!errors.isEmpty()) {
     return res.status(400).json({errors: errors.mapped()})
   }
@@ -94,19 +94,18 @@ app.post(
   [
     body('username').optional().matches(/[-_a-zA-Z0-9.]+/).isLength({min: 3}),
     body('password').optional().isString().isLength({min: 3}),
-    validate,
+    validate
   ],
   (req, res) => {
     credentials({username: req.body.username, password: req.body.password}, users, (err, credentials) => {
+      if (err) return res.status(500).end()
       login(credentials, users, (err, user) => {
         if (err && err.error === 'wrong-password') {
           return res.status(401).json({
             error: 'Wrong password, if you tried to create an account then the username is already taken'
           })
         }
-        if (err) {
-          return res.status(500).end()
-        }
+        if (err) return res.status(500).end()
         res.status(200).cookie('token', user._id).json({...user, token: user._id})
       })
     })
@@ -124,7 +123,7 @@ app.post(
     const message = {
       username: req.authenticatedUser.username,
       message: req.body.message,
-      room: req.body.room || 'main',
+      room: req.body.room || 'main'
     }
     // TODO: remove duplication of access to a room
     rooms.findOne({name: message.room}, (err, room) => {
@@ -164,7 +163,7 @@ app.post(
     const roomToCreate = {
       name: req.body.name,
       topic: req.body.topic,
-      isPrivate: req.body.isPrivate || false,
+      isPrivate: req.body.isPrivate || false
     }
     rooms.findOne({name: roomToCreate.name}, (err, roomFound) => {
       if (err) return res.status(500).end()
@@ -188,7 +187,7 @@ app.post(
   authenticate,
   [
     body('username').optional().matches(/[-_a-zA-Z0-9.]+/).isLength({min: 3}),
-    validate,
+    validate
   ],
   (req, res) => {
     // TODO: remove duplication of access to a room
@@ -207,6 +206,7 @@ app.post(
 
 app.get('/rooms', authenticate, (req, res) => {
   rooms.find({$or: [{isPrivate: false}, {isPrivate: true, name: {$in: req.authenticatedUser.rooms}}]}, (err, rooms) => {
+    if (err) return res.status(500).end()
     res.status(200).json({
       rooms: rooms.map(({name, topic, isPrivate}) => ({name, topic, isPrivate}))
     })
@@ -230,9 +230,9 @@ app.get('/rooms/:room/messages', authenticate, (req, res) => {
     if (!room) return res.status(404).end()
     if (room.isPrivate && !req.authenticatedUser.rooms.includes(room.name)) return res.status(401).json({error: 'Room is private'})
     const allMessagesInRoom = {room: room.name}
-    const matchingMessages = (req.query.search) ?
-          {$and: req.query.search.split('+').map(term => ({message: new RegExp(`\\b${term}\\b`)}))} :
-          {}
+    const matchingMessages = (req.query.search)
+      ? {$and: req.query.search.split('+').map(term => ({message: new RegExp(`\\b${term}\\b`)}))}
+      : {}
     const wantedMessages = {$and: [allMessagesInRoom, matchingMessages]}
     messages.find(wantedMessages).sort({createdAt: 1}).exec((err, messages) => {
       if (err) return res.status(500).end()
@@ -248,9 +248,9 @@ app.get('/messages', authenticate, (req, res) => {
   rooms.find(accessibleRooms, (err, rooms) => {
     if (err) return res.status(500).end()
     const allMessages = {room: {$in: rooms.map(({name}) => name)}}
-    const matchingMessages = (req.query.search) ?
-          {$and: req.query.search.split('+').map(term => ({message: new RegExp(`\\b${term}\\b`)}))} :
-          {}
+    const matchingMessages = (req.query.search)
+      ? {$and: req.query.search.split('+').map(term => ({message: new RegExp(`\\b${term}\\b`)}))}
+      : {}
     const wantedMessages = {$and: [allMessages, matchingMessages]}
     messages.find(wantedMessages).sort({createdAt: 1}).exec((err, messages) => {
       if (err) return res.status(500).end()
@@ -273,10 +273,11 @@ app.put(
   authenticate,
   [
     body('preferences').exists().custom((value) => typeof value === 'object' && value.constructor === Object),
-    validate,
+    validate
   ],
   (req, res) => {
     users.findOne({$or: [{username: req.params.user}, {_id: req.params.user}]}, (err, user) => {
+      if (err) return res.status(500).end()
       if (!user) return res.status(404).end()
       if (req.authenticatedUser._id !== user._id) return res.status(401).end()
       users.update({_id: user._id}, {$set: {preferences: req.body.preferences}}, {}, (err) => {
@@ -288,6 +289,7 @@ app.put(
 
 app.get('/users/:user/preferences', authenticate, (req, res) => {
   users.findOne({$or: [{username: req.params.user}, {_id: req.params.user}]}, (err, user) => {
+    if (err) return res.status(500).end()
     if (!user) return res.status(404).end()
     if (req.authenticatedUser._id !== user._id) return res.status(401).end()
     res.status(200).json(user.preferences)
@@ -299,10 +301,11 @@ app.put(
   authenticate,
   [
     body('profile').exists().custom((value) => typeof value === 'object' && value.constructor === Object),
-    validate,
+    validate
   ],
   (req, res) => {
     users.findOne({$or: [{username: req.params.user}, {_id: req.params.user}]}, (err, user) => {
+      if (err) return res.status(500).end()
       if (!user) return res.status(404).end()
       if (req.authenticatedUser._id !== user._id) return res.status(401).end()
       users.update({_id: user._id}, {$set: {profile: req.body.profile}}, {}, (err) => {
@@ -315,12 +318,14 @@ app.put(
 io.origins('*:*')
 
 io.use((socket, next) => {
-  const token = socket.handshake.query.token;
+  const token = socket.handshake.query.token
   users.findOne({_id: token}, (err, user) => {
     if (err) return next(err)
-    if (!user) return next(new Error(
-      'Authentication failed, token in not associated to any known user, please try to login again'
-    ));
+    if (!user) {
+      return next(new Error(
+        'Authentication failed, token in not associated to any known user, please try to login again'
+      ))
+    }
     // create association between token and socket
     // TODO: extract
     let socketsForUser = sockets.get(token) || []
@@ -331,12 +336,12 @@ io.use((socket, next) => {
         user: {
           username: user.username,
           profile: user.profile,
-          isConnected: true,
+          isConnected: true
         },
         event: 'user-is-online'
       })
     }
-    return next();
+    return next()
   })
 })
 
@@ -347,7 +352,7 @@ io.on('connection', socket => {
     console.log('socket disconnected', {token: socket.handshake.query.token, id: socket.id})
     // remove association between token and socket
     // TODO: extract
-    const token = socket.handshake.query.token;
+    const token = socket.handshake.query.token
     let socketsForUser = sockets.get(token) || []
     socketsForUser = socketsForUser.filter(socketForUser => socketForUser.id !== socket.id)
     sockets.set(token, socketsForUser)
@@ -360,7 +365,7 @@ io.on('connection', socket => {
             user: {
               username: user.username,
               profile: user.profile,
-              isConnected: false,
+              isConnected: false
             },
             event: 'user-went-offline'
           })
@@ -371,6 +376,10 @@ io.on('connection', socket => {
 })
 
 rooms.insert({name: 'main', topic: 'main', isPrivate: false}, (err, _room) => {
+  if (err) {
+    console.log('Unable to start server', err)
+    process.exit(1)
+  }
   server.listen(4000, () => {
     console.log('The server is running: http://localhost:4000')
   })
